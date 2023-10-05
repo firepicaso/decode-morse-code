9981d20ec4a7b8a7f7ba3313de7dcf4295b8c3d9
unknown <albertkantwi@gmail.com>
2023-10-05 23:10:47 +0100

    Update code

--- decode.rb
+++ decode.rb
@@ -6,19 +6,3 @@ MORSE_CODE = {
   '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
   '--..' => 'Z'
 }.freeze
-
-def decode_char(char)
-  MORSE_CODE[char]
-end
-
-def decode_word(word)
-  word.split.map { |char| decode_char(char) }.join
-end
-
-def decode(message)
-  message.split('   ').map { |word| decode_word(word) }.join(' ')
-end
-
-message_from_bottle = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
-
-print decode(message_from_bottle)
